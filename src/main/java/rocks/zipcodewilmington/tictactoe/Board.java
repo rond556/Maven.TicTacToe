40d75f028864703d9f76d68fe036fe02bc6608a6
Ron Duwell <ronduwell@zipcoders-MacBook-Pro.local>
2019-10-22 22:38:10 -0400

    played around a bit

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -5,18 +5,54 @@ package rocks.zipcodewilmington.tictactoe;
  */
 public class Board {
     public Board(Character[][] matrix) {
+
+        Character[][] board = new matrix;
     }
+
+    public TicTacToeBoard(Character victorySymbol) {
+        Board[][] board = new Board[][];
+        Character xOrO = victorySymbol;
+        //horizontal victory
+        if (board[0][0].equals(xOrO) && board[0][1].equals(xOrO) && board[0][2].equals(xOrO))
+            return true;
+        else if (board[1][0].equals(xOrO) && board[1][1].equals(xOrO) && board[1][2].equals(xOrO))
+            return true;
+        else if (board[2][0].equals(xOrO) && board[2][1].equals(xOrO) && board[2][2].equals(xOrO))
+            return true;
+            //vertical victory
+        else if (board[0][0].equals(xOrO) && board[1][0].equals(xOrO) && board[2][0].equals(xOrO))
+            return true;
+        else if (board[1][0].equals(xOrO) && board[1][1].equals(xOrO) && board[2][1].equals(xOrO))
+            return true;
+        else if (board[0][2].equals(xOrO) && board[1][2].equals(xOrO) && board[2][2].equals(xOrO))
+            return true;
+            //diagonal victory
+        else if (board[0][0].equals(xOrO) && board[1][1].equals(xOrO) && board[2][2].equals(xOrO))
+            return true;
+        else if (board[0][2].equals(xOrO) && board[1][1].equals(xOrO) && board[2][0].equals(xOrO))
+            return true;
+        else
+            return false;
+    }
+
+
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        }
     }
+    
+    
 
     public Boolean isInFavorOfO() {
-        return null;
+
+            
     }
 
     public Boolean isTie() {
-        return null;
+        Board[][] board = new Board[][];
+
+        }
     }
 
     public String getWinner() {
